129966735c20b10820399236cd708c77a5ee96c7
Hadiah <162602824+ash2413@users.noreply.github.com>
2024-09-08 17:04:46 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,51 @@
+<?php
+    include "database.php";
+
+ if(isset($_POST["login"]))
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT * FROM akun Where username='$username'
+        AND password='$password'";
+
+        $sql2 = "SELECT * FROM guru Where username2='$username'
+        AND password2='$password'";
+
+        $sql3 = "SELECT * FROM adm Where username3='$username'
+        AND password3='$password'";
+    
+        $result = $db->query($sql);
+
+        $relt = $db->query($sql2);
+
+        $rlt = $db->query($sql3);
+
+      
+        if ($result->num_rows > 0) {
+            $data = $result->fetch_assoc();
+            header("location: murid.html");
+
+        }else {
+            ;
+        }
+
+        if ($relt->num_rows > 0) {
+            $dt = $relt->fetch_assoc();
+            header("location: guru.html");
+
+        }else {
+            ;
+        }
+
+        if ($rlt->num_rows > 0) {
+            $dat = $rlt->fetch_assoc();
+            header("location: admin.html");
+
+        }else {
+            ;
+        }
+
+        
+    }
+    ?>
